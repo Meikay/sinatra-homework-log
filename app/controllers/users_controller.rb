e8c82749e9e09ea83250e36e0afd95bd006870ab
meisuleen <meikay@github.com>
2019-09-06 10:10:31 -0400

    Finds and authenticates user from db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,22 @@ class UsersController < ApplicationController
       erb :'/users/login'  
     end
 
+    # recieves the login form,  
+    post '/login' do
+      # finds the user,
+      @user = User.find_by(username: params[:username])
+      # authenticates the user
+      if @user.authenticate(params[:password])
+        # logs the user in a session - goes into application_controller for enabling sessions
+        # redirects to index/show page
+        binding.pry
+      else
+        # tell user they entered incorrect username/password
+        # redirect them to login
+      end
+    end
+
+
     #routes for signup
     get '/signup' do
       erb :'/users/signup'
