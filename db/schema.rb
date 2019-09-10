28e892e3055ce177879f64855212f02511257e54
meisuleen <meikay@github.com>
2019-09-10 11:23:14 -0400

    Remove :date from DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190901031233) do
+ActiveRecord::Schema.define(version: 20190910151544) do
 
   create_table "homework_assignments", force: :cascade do |t|
     t.string   "subject"
     t.string   "notes"
-    t.integer  "date"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@
 alice = User.create(username: "alice", password: "wonderland")
 prince = User.create(username: "prince", password: "arabia")
 
-HomeworkAssignment.create(subject: "Biology", notes: "Complete section 5 and answer section questions.", date: "October 15, 2019", user_id: "alice.id")
-HomeworkAssignment.create(subject: "Physics", notes: "Study for chapter 4 test", date: "October 15, 2019", user_id: "prince.id")
+HomeworkAssignment.create(subject: "Biology", notes: "Complete section 5 and answer section questions.", date: "October 15, 2019", user_id: alice.id)
+HomeworkAssignment.create(subject: "Physics", notes: "Study for chapter 4 test", date: "October 15, 2019", user_id: prince.id)
 
 #Use AR to pre-associate data:
 alice.homework_assignment.create(notes: "We are building a project!")
